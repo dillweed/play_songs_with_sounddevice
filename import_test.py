a39c7c705ce6eb88c508a30241f367eac94ba240
Dill <4053467+dillweed@users.noreply.github.com>
2023-08-26 23:25:20 -0700

    add random song function to import_test

--- import_test.py
+++ import_test.py
@@ -1,7 +1,7 @@
 # Import play_songs_with_sounddevice.py and play a song.
 import play_songs_with_sounddevice
 
-# play_songs_with_sounddevice.play_random_song()
+play_songs_with_sounddevice.play_random_song()
 play_songs_with_sounddevice.play_song("empire")
 play_songs_with_sounddevice.play_song("scale")
 play_songs_with_sounddevice.play_song("range")
