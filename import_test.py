b456b0747646193fd5be4f308bfbc99f83b46643
Dill <4053467+dillweed@users.noreply.github.com>
2023-08-26 23:02:25 -0700

    remove import_test.py from .gitignore

---
+++ import_test.py
@@ -0,0 +1,8 @@
+# Import play_songs_with_sounddevice.py and play a song.
+import play_songs_with_sounddevice
+
+# play_songs_with_sounddevice.play_random_song()
+play_songs_with_sounddevice.play_song("empire")
+play_songs_with_sounddevice.play_song("scale")
+play_songs_with_sounddevice.play_song("range")
+play_songs_with_sounddevice.play_song("sweet_child")
